a1f0e9442bf53787e170e7f8605671023dafb863
GeorgeGogoladze <114743204+GeorgeGogoladze@users.noreply.github.com>
2022-11-18 18:07:34 +0400

    added javascript part

---
+++ app.js
@@ -0,0 +1,63 @@
+const numbers = [2, 8, 6, 10, 20];
+const sum = numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4];
+const length = numbers.length;
+
+const average = sum / length;
+
+console.log(average);
+
+
+const student = [
+	{
+		name: "nino",
+		age: 25,
+	},
+	{
+		name: "tamari",
+		age: 23,
+	},
+	{
+		name: "giorgi",
+		age: 25,
+	},
+	{
+		name: "nika",
+		age: 25,
+	},
+	{
+		name: "lizi",
+		age: 27,
+	},
+
+];
+
+
+const person = {
+	firstName: "giorgi",
+	lastNamae: "gogoladze",
+	age: 25,
+	email: "example@.com",
+	phoneNumbers: [598898767, 597959059],
+};
+
+
+const text = `user name is ${person.firstName} user age is ${person.age}, user phone 
+number is ${person.phoneNumbers[0]}`;
+
+console.log(text);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
